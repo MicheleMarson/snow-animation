ea1cbc30c123b9f64423041dd9ff00a292fc5925
Mihaela Marson <mitchmarson666@gmail.com>
2021-09-22 11:16:17 +0200

    add random colors

--- script.js
+++ script.js
@@ -32,18 +32,21 @@ class Particle{
   constructor(){
     // this.x = mouse.x
     // this.y = mouse.y
+    // this.random = Math.round(Math.random()*255)
     this.x = Math.random() * canvas.width
     this.y = Math.random() * canvas.height
     this.size = Math.random() * 5 + 1
     this.speedX = Math.random() * 3 - 1.5
     this.speedY = Math.random() * 3 - 1.5
+    this.color = "rgb("+Math.round(Math.random()*255) +","
+    +Math.round(Math.random()*255) +","+Math.round(Math.random()*255)+")"
   }
   update(){
     this.x += this.speedX
     this.y += this.speedY
   }
   draw(){
-    ctx.fillStyle = "white"
+    ctx.fillStyle = this.color
     ctx.beginPath()
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
     ctx.fill()
