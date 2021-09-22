fe93a31d8d9c4dc4840bbaed27f701e7facf1116
Mihaela Marson <mitchmarson666@gmail.com>
2021-09-22 11:09:27 +0200

    adding files

---
+++ script.js
@@ -0,0 +1,75 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+const particlesArray = []
+// console.log(ctx);
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+})
+
+const mouse = {
+  y: 100,
+  x: 100,
+}
+
+// canvas.addEventListener("click", (e) => {
+//   mouse.x = e.x
+//   mouse.y = e.y
+//   draw()
+// })
+
+canvas.addEventListener("mousemove", (e) => {
+  mouse.x = e.x
+  mouse.y = e.y
+  // draw()
+  
+})
+
+class Particle{
+  constructor(){
+    // this.x = mouse.x
+    // this.y = mouse.y
+    this.x = Math.random() * canvas.width
+    this.y = Math.random() * canvas.height
+    this.size = Math.random() * 5 + 1
+    this.speedX = Math.random() * 3 - 1.5
+    this.speedY = Math.random() * 3 - 1.5
+  }
+  update(){
+    this.x += this.speedX
+    this.y += this.speedY
+  }
+  draw(){
+    ctx.fillStyle = "white"
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+    ctx.fill()
+  }
+}
+
+function init(){
+  for(let i = 0; i < 100; i++){
+    particlesArray.push(new Particle())
+  }
+}
+
+init()
+
+function handleParticles(){
+  for(let i = 0; i < particlesArray.length; i++){
+    particlesArray[i].update()
+    particlesArray[i].draw()
+  }
+}
+
+function animate(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  handleParticles()
+  requestAnimationFrame(animate) //calls function we pass in - creates a loop
+}
+
+animate()
+
