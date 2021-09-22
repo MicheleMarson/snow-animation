c67212f2cd5c98b16818518e0fa861dcc4f92cd6
Mihaela Marson <mitchmarson666@gmail.com>
2021-09-22 11:44:38 +0200

    color-change

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 const particlesArray = []
-// console.log(ctx);
+let hue = 0
 
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth
@@ -18,32 +18,34 @@ const mouse = {
 // canvas.addEventListener("click", (e) => {
 //   mouse.x = e.x
 //   mouse.y = e.y
-//   draw()
+//   for(let i = 0; i < 20; i++){ 
+//     particlesArray.push(new Particle()) // on click add 20 particles
+//   }
 // })
 
 canvas.addEventListener("mousemove", (e) => {
   mouse.x = e.x
   mouse.y = e.y
   // draw()
-  
+  // particlesArray.push(new Particle()) // add particles on mouse move
+  for(let i = 0; i < 10; i++){ 
+    particlesArray.push(new Particle()) // on click add 20 particles
+  }
 })
 
 class Particle{
   constructor(){
-    // this.x = mouse.x
-    // this.y = mouse.y
-    // this.random = Math.round(Math.random()*255)
-    this.x = Math.random() * canvas.width
-    this.y = Math.random() * canvas.height
+    this.x = mouse.x
+    this.y = mouse.y
     this.size = Math.random() * 5 + 1
     this.speedX = Math.random() * 3 - 1.5
     this.speedY = Math.random() * 3 - 1.5
-    this.color = "rgb("+Math.round(Math.random()*255) +","
-    +Math.round(Math.random()*255) +","+Math.round(Math.random()*255)+")"
+    this.color = "hsl("+hue+",100%,50%)"
   }
   update(){
     this.x += this.speedX
     this.y += this.speedY
+    if(this.size > 0.2) this.size -= 0.01 // shrink the particles as they move away
   }
   draw(){
     ctx.fillStyle = this.color
@@ -53,23 +55,23 @@ class Particle{
   }
 }
 
-function init(){
-  for(let i = 0; i < 300; i++){
-    particlesArray.push(new Particle())
-  }
-}
-
-init()
-
 function handleParticles(){
   for(let i = 0; i < particlesArray.length; i++){
     particlesArray[i].update()
     particlesArray[i].draw()
+    if(particlesArray[i].size <= 0.3){ // if size is bigger than .3 remove one element
+      particlesArray.splice(i, 1)
+      console.log(particlesArray.length);
+      i--
+    }
   }
 }
 
 function animate(){
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.clearRect(0, 0, canvas.width, canvas.height) // clear the canvas on every draw
+  // ctx.fillStyle = "rgba(0,0,0,.1)" // add trails to particles
+  // ctx.fillRect(0,0,canvas.width, canvas.height)
+  hue++
   handleParticles()
   requestAnimationFrame(animate) //calls function we pass in - creates a loop
 }
